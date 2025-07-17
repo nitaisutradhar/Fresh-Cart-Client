fd8ee36d737c47535bd3bcfecf318b7895b937f1
nitaisutradhar <softbeezoffice@gmail.com>
2025-07-18 01:49:44 +0600

    add private route

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,14 @@
+import Loading from '@/components/Loading'
+import useAuth from '../hooks/useAuth'
+import { Navigate, useLocation } from 'react-router'
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth()
+  const location = useLocation()
+
+  if (loading) return <Loading />
+  if (user) return children
+  return <Navigate to='/login' state={{ from: location }} replace='true' />
+}
+
+export default PrivateRoute
